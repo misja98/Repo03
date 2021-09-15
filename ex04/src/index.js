5be558ddf6dd8896837144bb3d253dabeffec012
misja98 <mirko.djukic@icloud.com>
2021-09-15 21:49:13 +0200

    Adding forth example

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+  var x;
+  var y;
+  x = 10;
+  x = y;
+    //Only change code above this line
+    return [x, y];
+}
+
+console.log(main());
+module.exports = main;
