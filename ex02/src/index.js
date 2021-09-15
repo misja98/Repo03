27bda0bd74b0db313a58abc7f3a3e64215c0ea78
misja98 <mirko.djukic@icloud.com>
2021-09-15 21:19:17 +0200

    Adding second example

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+    //New comment
+    /* Comment number
+    two */
+    //Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
